7a4504e7d218ce3db59fd3e1890edfc0e6cbdc50
Mihai Vladu <mihai@academicmerit.com>
2018-03-29 16:15:07 +0300

    Updated admin.py so app's models work.

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from app.models import *
 
 # Register your models here.
-admin.site.register(User, User)
-admin.site.register(Post, Post)
-admin.site.register(Video, Video)
-admin.site.register(Comment, Comment)
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Video)
+admin.site.register(Comment)
